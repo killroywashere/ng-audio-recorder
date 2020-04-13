c9f6e6b878b8ba2a020f91879b254561a576debe
Kishin Karra <kishinkarra@gmail.com>
2020-04-13 23:05:09 +0530

    Pause functionality

--- src/lib/ng-audio-recorder.service.ts
+++ src/lib/ng-audio-recorder.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { timeout } from 'rxjs/operators';
 
 declare var MediaRecorder: any;
 
@@ -6,39 +7,71 @@ declare var MediaRecorder: any;
   providedIn: 'root'
 })
 export class NgAudioRecorderService {
+  get recorderState(): RecorderState {
+    return this._recorderState;
+  }
+
   private chunks: Array<any> = [];
   protected recorderEnded = new EventEmitter();
+  recorderError = new EventEmitter<ErrorCase>();
+  // tslint:disable-next-line
+  private _recorderState = RecorderState.INITIALIZING;
 
   constructor() {
   }
 
   private recorder: any;
 
+
   private static getUserConsent() {
     return navigator.mediaDevices.getUserMedia({audio: true});
   }
 
   startRecording() {
+    if (this._recorderState === RecorderState.RECORDING) {
+      this.recorderError.emit(ErrorCase.ALREADY_RECORDING);
+    }
+    if (this._recorderState === RecorderState.PAUSED) {
+      this.resume();
+      return;
+    }
+    this._recorderState = RecorderState.INITIALIZING;
     NgAudioRecorderService.getUserConsent().then((mediaStream) => {
       this.recorder = new MediaRecorder(mediaStream);
+      this._recorderState = RecorderState.INITIALIZED;
       this.addListeners();
       this.recorder.start();
+      this._recorderState = RecorderState.RECORDING;
     });
+  }
+
+  resume() {
+    if (this._recorderState === RecorderState.PAUSED) {
+      this._recorderState = RecorderState.RECORDING;
+      this.recorder.resume();
+    }
   }
 
 
   stopRecording(outputFormat: OutputFormat) {
+    this._recorderState = RecorderState.STOPPING;
     return new Promise((resolve, reject) => {
-      this.recorderEnded.subscribe((blob) => {
+      this.recorderEnded.pipe(timeout(2000)).subscribe((blob) => {
+        this._recorderState = RecorderState.STOPPED;
         if (outputFormat === OutputFormat.BLOB) {
           resolve(blob);
         }
-        if (outputFormat === OutputFormat.BLOB_URL) {
+        if (outputFormat === OutputFormat.WEBM_BLOB_URL) {
           const audioURL = URL.createObjectURL(blob);
           resolve(audioURL);
         }
+      }, _ => {
+        this.recorderError.emit(ErrorCase.RECORDER_TIMEOUT);
+        reject(ErrorCase.RECORDER_TIMEOUT);
       });
       this.recorder.stop();
+    }).catch(() => {
+      this.recorderError.emit(ErrorCase.USER_CONSENT_FAILED);
     });
   }
 
@@ -66,6 +99,21 @@ export class NgAudioRecorderService {
 
 
 export enum OutputFormat {
-  BLOB_URL,
+  WEBM_BLOB_URL,
   BLOB,
+}
+
+export enum ErrorCase {
+  USER_CONSENT_FAILED,
+  RECORDER_TIMEOUT,
+  ALREADY_RECORDING
+}
+
+export enum RecorderState {
+  INITIALIZING,
+  INITIALIZED,
+  RECORDING,
+  PAUSED,
+  STOPPING,
+  STOPPED
 }
