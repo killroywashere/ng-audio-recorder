f989ad0f7a541d31931e849d050f9cb455c06ca2
Lucas Alderete <lucasm.alderete@gmail.com>
2022-02-19 23:15:01 -0300

    Added recorderState Emitter
    Added recorderState for subscribes when change state

--- src/lib/ng-audio-recorder.service.ts
+++ src/lib/ng-audio-recorder.service.ts
@@ -5,123 +5,130 @@ declare var MediaRecorder: any;
 @Injectable()
 export class NgAudioRecorderService {
 
-  private chunks: Array<any> = [];
-  protected recorderEnded = new EventEmitter();
-  public recorderError = new EventEmitter<ErrorCase>();
-  // tslint:disable-next-line
-  private _recorderState = RecorderState.INITIALIZING;
+	private chunks: Array<any> = [];
+	public recorderState = new EventEmitter<RecorderState>();
+	protected recorderEnded = new EventEmitter();
+	public recorderError = new EventEmitter<ErrorCase>();
+	// tslint:disable-next-line
+	private _recorderState = RecorderState.INITIALIZING;
 
-  constructor() {
-  }
+	constructor() {
+	}
 
-  private recorder: any;
-
-
-  private static guc() {
-    return navigator.mediaDevices.getUserMedia({audio: true});
-  }
+	private recorder: any;
 
 
-  getUserContent() {
-    return NgAudioRecorderService.guc();
-  }
+	private static guc() {
+		return navigator.mediaDevices.getUserMedia({ audio: true });
+	}
 
-  startRecording() {
-    if (this._recorderState === RecorderState.RECORDING) {
-      this.recorderError.emit(ErrorCase.ALREADY_RECORDING);
-    }
-    if (this._recorderState === RecorderState.PAUSED) {
-      this.resume();
-      return;
-    }
-    this._recorderState = RecorderState.INITIALIZING;
-    NgAudioRecorderService.guc().then((mediaStream) => {
-      this.recorder = new MediaRecorder(mediaStream);
-      this._recorderState = RecorderState.INITIALIZED;
-      this.addListeners();
-      this.recorder.start();
-      this._recorderState = RecorderState.RECORDING;
-    });
-  }
 
-  pause() {
-    if (this._recorderState === RecorderState.RECORDING) {
-      this.recorder.pause();
-      this._recorderState = RecorderState.PAUSED;
-    }
-  }
+	getUserContent() {
+		return NgAudioRecorder80CustomService.guc();
+	}
 
-  resume() {
-    if (this._recorderState === RecorderState.PAUSED) {
-      this._recorderState = RecorderState.RECORDING;
-      this.recorder.resume();
-    }
-  }
+	startRecording() {
+		if (this._recorderState === RecorderState.RECORDING) {
+			this.recorderError.emit(ErrorCase.ALREADY_RECORDING);
+		}
+		if (this._recorderState === RecorderState.PAUSED) {
+			this.resume();
+			return;
+		}
+		this._recorderState = RecorderState.INITIALIZING;
+		NgAudioRecorder80CustomService.guc().then((mediaStream) => {
+			this.recorder = new MediaRecorder(mediaStream);
+			this._recorderState = RecorderState.INITIALIZED;
+			this.recorderState.emit(this._recorderState)
+			this.addListeners();
+			this.recorder.start();
+			this._recorderState = RecorderState.RECORDING;
+			this.recorderState.emit(this._recorderState)
+		});
+	}
 
-  stopRecording(outputFormat: OutputFormat) {
-    this._recorderState = RecorderState.STOPPING;
-    return new Promise((resolve, reject) => {
-      this.recorderEnded.subscribe((blob) => {
-        this._recorderState = RecorderState.STOPPED;
-        if (outputFormat === OutputFormat.WEBM_BLOB) {
-          resolve(blob);
-        }
-        if (outputFormat === OutputFormat.WEBM_BLOB_URL) {
-          const audioURL = URL.createObjectURL(blob);
-          resolve(audioURL);
-        }
-      }, _ => {
-        this.recorderError.emit(ErrorCase.RECORDER_TIMEOUT);
-        reject(ErrorCase.RECORDER_TIMEOUT);
-      });
-      this.recorder.stop();
-    }).catch(() => {
-      this.recorderError.emit(ErrorCase.USER_CONSENT_FAILED);
-    });
-  }
+	pause() {
+		if (this._recorderState === RecorderState.RECORDING) {
+			this.recorder.pause();
+			this._recorderState = RecorderState.PAUSED;
+			this.recorderState.emit(this._recorderState)
+		}
+	}
 
-  getRecorderState() {
-    return this._recorderState;
-  }
+	resume() {
+		if (this._recorderState === RecorderState.PAUSED) {
+			this._recorderState = RecorderState.RECORDING;
+			this.recorderState.emit(this._recorderState)
+			this.recorder.resume();
+		}
+	}
 
-  private addListeners() {
-    this.recorder.ondataavailable = this.appendToChunks;
-    this.recorder.onstop = this.recordingStopped;
-  }
+	stopRecording(outputFormat: OutputFormat) {
+		this._recorderState = RecorderState.STOPPING;
+		this.recorderState.emit(this._recorderState)
+		return new Promise((resolve, reject) => {
+			this.recorderEnded.subscribe((blob) => {
+				this._recorderState = RecorderState.STOPPED;
+				this.recorderState.emit(this._recorderState)
+				if (outputFormat === OutputFormat.WEBM_BLOB) {
+					resolve(blob);
+				}
+				if (outputFormat === OutputFormat.WEBM_BLOB_URL) {
+					const audioURL = URL.createObjectURL(blob);
+					resolve(audioURL);
+				}
+			}, _ => {
+				this.recorderError.emit(ErrorCase.RECORDER_TIMEOUT);
+				reject(ErrorCase.RECORDER_TIMEOUT);
+			});
+			this.recorder.stop();
+		}).catch(() => {
+			this.recorderError.emit(ErrorCase.USER_CONSENT_FAILED);
+		});
+	}
 
-  private appendToChunks = (event: any) => {
-    this.chunks.push(event.data);
-  };
-  private recordingStopped = (event: any) => {
-    const blob = new Blob(this.chunks, {type: 'audio/webm'});
-    this.chunks = [];
-    this.recorderEnded.emit(blob);
-    this.clear();
-  };
+	getRecorderState() {
+		return this._recorderState;
+	}
 
-  private clear() {
-    this.recorder = null;
-    this.chunks = [];
-  }
+	private addListeners() {
+		this.recorder.ondataavailable = this.appendToChunks;
+		this.recorder.onstop = this.recordingStopped;
+	}
+
+	private appendToChunks = (event: any) => {
+		this.chunks.push(event.data);
+	};
+	private recordingStopped = (event: any) => {
+		const blob = new Blob(this.chunks, { type: 'audio/webm' });
+		this.chunks = [];
+		this.recorderEnded.emit(blob);
+		this.clear();
+	};
+
+	private clear() {
+		this.recorder = null;
+		this.chunks = [];
+	}
 }
 
 
 export enum OutputFormat {
-  WEBM_BLOB_URL,
-  WEBM_BLOB,
+	WEBM_BLOB_URL,
+	WEBM_BLOB,
 }
 
 export enum ErrorCase {
-  USER_CONSENT_FAILED,
-  RECORDER_TIMEOUT,
-  ALREADY_RECORDING
+	USER_CONSENT_FAILED,
+	RECORDER_TIMEOUT,
+	ALREADY_RECORDING
 }
 
 export enum RecorderState {
-  INITIALIZING,
-  INITIALIZED,
-  RECORDING,
-  PAUSED,
-  STOPPING,
-  STOPPED
+	INITIALIZING,
+	INITIALIZED,
+	RECORDING,
+	PAUSED,
+	STOPPING,
+	STOPPED
 }
